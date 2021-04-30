c9784a1a3e7371b3cb249584e477c70ce11df48a
jbeard <player69>
2021-04-30 01:24:46 -0700

    deploying

--- public/js/idb.js
+++ public/js/idb.js
@@ -1,22 +1,27 @@
-// ** FILE FORMATTED WITH @ext:esbenp.prettier-vscode **
-
+// variable that holds the db connection
 let db;
+// establishes a connection the IndexedDB db
 const request = indexedDB.open('budget-tracker', 1);
-
+// this event occurs if the db version changes
 request.onupgradeneeded = function (event) {
+  // save ref to the db
   const db = event.target.result;
+  // create an object store
   db.createObjectStore('new_budget', { autoIncrement: true });
 };
 
-request.onsuccess = function (event) {
+// when succesful:
+request.onsuccess = function(event) {
+  // when db is create with its obj store
   db = event.target.result;
 
+  // check if app is online
   if (navigator.onLine) {
     uploadBudget();
   }
 };
 
-request.onerror = function (event) {
+request.onerror = function(event) {
   console.log(event.target.errorCode);
 };
 
@@ -24,15 +29,19 @@ function saveRecord(record) {
   const transaction = db.transaction(['new_budget'], 'readwrite');
 
   const budgetObjectStore = transaction.objectStore('new_budget');
-
+  
   budgetObjectStore.add(record);
 };
 
 function uploadBudget() {
+  // open transaction to db
   const transaction = db.transaction(['new_budget'], 'readwrite');
+  // access obj store
   const budgetObjectStore = transaction.objectStore('new_budget');
+  // get all records from store and set to variable
   const getAll = budgetObjectStore.getAll();
-  getAll.onsuccess = function () {
+  //upon successful .getAll():
+  getAll.onsuccess = function() {
     if (getAll.result.length > 0) {
       fetch('/api/transaction', {
         method: 'POST',
@@ -42,22 +51,26 @@ function uploadBudget() {
           'Content-Type': 'application/json'
         }
       })
-        .then(res => res.json())
-        .then(serverRes => {
-          if (serverRes.message) {
-            throw new Error(serverRes);
-          }
-          const transaction = db.transaction(['new_budget'], 'readwrite');
-          const budgetObjectStore = transaction.objectStore('new_budget');
-          budgetObjectStore.clear();
+      .then(res => res.json())
+      .then(serverRes => {
+        if (serverRes.message) {
+          throw new Error(serverRes);
+        }
+        // open another transaction
+        const transaction = db.transaction(['new_budget'], 'readwrite');
+        // access new_budget obj store
+        const budgetObjectStore = transaction.objectStore('new_budget');
+        // clear all items in the store
+        budgetObjectStore.clear();
 
-          alert('All of your saved transactions have been submitted.')
-        })
-        .catch(err => {
-          console.log(err)
-        });
+        alert('All of your saved transactions have been submitted.')
+      })
+      .catch(err => {
+        console.log(err)
+      });
     }
   };
 }
 
+// listen for the app coming back online
 window.addEventListener('online', uploadBudget);
--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-const express = require("express"); 
+const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3003;
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
 
 const app = express();
@@ -16,9 +16,9 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/budget', {
-  useFindAndModify: false,
+mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
+  useFindAndModify: false,
   useUnifiedTopology: true
 });
 
@@ -26,5 +26,5 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/budget', {
 app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
+  console.log(`App running on port http://localhost:${PORT}`);
 });
